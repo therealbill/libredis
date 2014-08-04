c072f280755386efc811fb50006c9ddfe8475827
Bill Anderson <therealbill@me.com>
2014-08-04 11:01:08 -0500

    IMport of codebase

---
+++ examples/README.md
@@ -0,0 +1,3 @@
+# Running the Samples
+
+# Each sample is intended to be ran by running `go run <samplename>.go`
---
+++ examples/redis_info.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	rclient "github.com/therealbill/libredis/client"
+)
+
+var (
+	network  = "tcp"
+	address  = "127.0.0.1:6379"
+	db       = 1
+	password = ""
+	timeout  = 5 * time.Second
+	maxidle  = 1
+	r        *rclient.Redis
+)
+
+func init() {
+	client, err := rclient.Dial("127.0.0.1", 6379)
+	if err != nil {
+		panic(err)
+	}
+	r = client
+}
+
+func main() {
+	all := r.GetAllInfo()
+	fmt.Printf("Redis Server Version: %s\n", all.Server.Version)
+	fmt.Printf("Redis Server Role: %s\n", all.Replication.Role)
+	if all.Replication.ConnectedSlaves > 0 {
+		fmt.Println("Slaves:")
+		fmt.Printf("\tNumber Connected: %d\n", all.Replication.ConnectedSlaves)
+		for _, slave := range all.Replication.Slaves {
+			fmt.Printf("\tSlave: %s\n", slave)
+		}
+	}
+	if all.Replication.Role == "slave" {
+		fmt.Printf("Master: %s:%d\n", all.Replication.MasterHost, all.Replication.MasterPort)
+	}
+
+	fmt.Printf("Redis Used Memory: %d\n", all.Memory.UsedMemory)
+	fmt.Printf("Redis Used Memory Human: %s\n", all.Memory.UsedMemoryHuman)
+
+}
